da21b00b372aa0364bb3b849b5a1468471de01e1
elsehow <yes@cosmopol.is>
2017-06-25 15:42:09 -0700

    fixed

--- README.md
+++ README.md
@@ -1,27 +1,14 @@
-# poloniex-value-estimator
-
-a [kefir stream](https://rpominov.github.io/kefir/) of your Poloniex account's value, in BTC and USD.
+# empty-module
 
 ## install
 
 ```
-git clone git@github.com:elsehow/poloniex-value-estimator.git
-cd poloniex-value-estimator
-npm i
 ```
 
-try an example (requires InfluxDB running):
+## use
 
+```js
 ```
-node example/influx.js
-```
-
-## TODO
-
-- method to fetch new balances
-- "pass through" per-coin balances, values + btc value
-- simple example (?)
-- restart on error (?)
 
 ## license
 
--- test/index.js
+++ test/index.js
@@ -1,25 +1,2 @@
-const fundEstimateS = require('..')
-const test = require('tape')
-
-test('offers BTC and USD estimates', t => {
-  const keys = require('../poloniex-keys.json')
-  const estS = fundEstimateS(
-    keys['pk'],
-    keys['sk']
-  )
-  function check (est) {
-    t.ok(est.btcValue)
-    t.deepEqual(typeof(est.btcValue), 'number')
-    t.ok(est.usdValue)
-    t.deepEqual(typeof(est.usdValue), 'number')
-    t.end()
-    estS.offValue(check)
-    estS.offError(err)
-  }
-  function err (error) {
-    t.notOk(err)
-  }
-  estS.onValue(check)
-  estS.onError(err)
-})
-
+const mod = require('..')
+console.log(mod)
