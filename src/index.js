63f4a2ac9a620689a1bc72d45b45ba434a4e431d
elsehow <yes@cosmopol.is>
2017-08-25 11:43:22 -0700

    initial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-module.exports = 'hello'
+const socket = require('socket.io')
+
+function server (port) {
+  const serv = require('http').createServer();
+  const io = socket(serv)
+
+  io.on('connection', client => {
+    console.log('client connected!')
+    client.on('disconnect', _ => {
+      console.log('client disconnected!')
+    })
+  })
+
+  serv.listen(port)
+
+  return serv
+}
+
+module.exports = {
+  server: server,
+}
--- test/index.js
+++ test/index.js
@@ -1,2 +1,27 @@
+const test = require('tape')
+const socketClient = require('socket.io-client')
 const mod = require('..')
-console.log(mod)
+
+// test config
+const serverPort = 3000
+const serverUrl = 'http://localhost:'+serverPort
+const ioServer = mod.server(serverPort)
+let socket = null; // we will assign this later
+
+test('can start server', t => {
+  t.ok(ioServer)
+  t.end()
+})
+
+test('can connect to server', t => {
+  socket = socketClient(serverUrl)
+  socket.on('connect', function () {
+    t.ok(true, 'connected to server')
+    t.end()
+  })
+})
+
+test.onFinish(_ => {
+  ioServer.close()
+  socket.close()
+})
